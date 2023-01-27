959525d8d40acc978c5dc40193b732723fa872a9
Fredricc <kfred45.fk@gmail.com>
2023-01-27 18:31:17 +0300

    Creating database and tables

---
+++ AppDataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace E_Library
+{
+    public class AppDataContext: DbContext
+    {
+        public AppDataContext(DbContextOptions<AppDataContext> options) 
+            : base(options) 
+        {
+        
+        }
+    }
+}

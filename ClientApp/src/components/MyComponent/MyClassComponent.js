d68a31a20d7a540e330d83a46d342943c705e96f
Fredricc <kfred45.fk@gmail.com>
2023-01-31 15:30:42 +0300

    React Component

---
+++ ClientApp/src/components/MyComponent/MyClassComponent.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from "react"
+
+export class MyComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h2>
+                    My Class Compontent
+                </h2>
+            </div>
+            )
+    }
+}
